97d65ed89af962dea3656d897128b733c58cf265
Rajat Agarwal <rajat@Rajats-MacBook-Pro.local>
2021-10-02 19:31:50 +0530

    Initial Commit

---
+++ src/components/form/form.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import './form.scss';
+
+export default function Form() {
+    return <div className="form">This is a React form</div>;
+}
---
+++ src/pages/login.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { Form } from '../components/form';
+
+export default function Login() {
+    return <Form />;
+}
